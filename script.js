06de833c9b30e3f063c6f50a699e41ab52636fc9
Arthur Pimenta <arthurstt22@gmail.com>
2022-01-28 15:41:55 -0300

    Wip

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Para chamar Id.
+/* // Para chamar Id.
 
 //const animais = document.getElementById('animais');
 //console.log(animais.innerText);
@@ -21,13 +21,42 @@ console.log(linkInterno);
 
 // querySelectorAll => Seleciona todos os elementos que possueirem a mesma Classe ou Id!
 
-const animaisImg = document.querySelectorAll('.animais img');
-console.log(animaisImg)
+//const animaisImg = document.querySelectorAll('.animais img');
+//console.log(animaisImg)
 
 // HTMLCollection vs NodeList
-// As mudanças feitas no HTMLCollection são atualiazidas, já as feitas com "querySelectorAll (NodeList)" não são feitas. Ex.:
+// As mudanças feitas no HTMLCollection são atualiazidas, já as feitas com "querySelectorAll (NodeList)" não são feitas. Ex.: 
 
-const gridSelectinHTML = document.getElementsByClassName('grid-selection');
-const gridSectionNode = document.querySelectorAll('.grid-selection');
+const titulo = document.querySelector('.titulo');
+const gridSectionHTML = document.getElementsByClassName('grid-section');
+const gridSectionNode = document.querySelectorAll('.grid-section');
 
-primeiraUl.classList.add('grid-section')
+titulo.classList.add('grid-section');
+
+console.log(gridSectionHTML); // 4 itens
+console.log(gridSectionNode); // 3 itens
+
+
+gridSectionNode.forEach(function(item, index){
+    console.log(index)
+}); */
+
+// Retorne no console todas as imagens do site
+
+const AllImg = document.querySelectorAll('img');
+
+
+// Retorne no console apenas as imagens que começaram com a palavra imagem
+
+const imagensInicial = document.querySelectorAll('img[src^="img/imagem"');
+
+// Selecione todos os links internos (onde o href começa com #)
+
+const linkInterno = document.querySelector('[href^="#"]');
+
+// Selecione o primeiro h2 dentro de .animais-descricao
+
+const kj = document.querySelectorAll('.animais-descricao h2');
+
+// Selecione o último p do site
+
