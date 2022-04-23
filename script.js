222be8afa87547574ce53c7cb116315e215b54be
Arthur Pimenta <arthurstt22@gmail.com>
2022-04-23 12:36:10 -0300

    data-set changes

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function initTabNav() {
-    const tabMenu = document.querySelectorAll('.js-tabmenu li');
-    const tabContent = document.querySelectorAll('.js-tabcontent section');
+    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
+    const tabContent = document.querySelectorAll('[data-tab="content"] section');
 
     if(tabContent.length && tabMenu.length) {
         tabContent[0].classList.add('ativo');
@@ -9,7 +9,9 @@ function initTabNav() {
             tabContent.forEach((section) => {
             section.classList.remove('ativo');
             });
-            tabContent[index].classList.add('ativo');
+
+            const direcao = tabContent[index].dataset.anime;
+            tabContent[index].classList.add('ativo', direcao);
         }
 
         tabMenu.forEach((itemMenu, index) => {
@@ -22,7 +24,7 @@ function initTabNav() {
 initTabNav();
 
 function initAccordion() {
-    const accordionList = document.querySelectorAll('.js-accordion dt');
+    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
     accordionList[0].classList.add('ativo');
     accordionList[0].nextElementSibling.classList.add('ativo');
 
@@ -38,7 +40,7 @@ function initAccordion() {
 initAccordion();
 
 function initscrollSuave() {
-    const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
 
     function scrollToSection(event) {
         event.preventDefault();
@@ -60,7 +62,7 @@ function initscrollSuave() {
 initscrollSuave();
 
 function initScroll() {
-    const sections = document.querySelectorAll('.js-scroll');
+    const sections = document.querySelectorAll('[data-anime="scroll"]');
     if(sections.length) {
         const windowMetade = window.innerHeight * 0.6;
 
