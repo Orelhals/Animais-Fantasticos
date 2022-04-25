a6dae8df53daa2d6763bdabb37382f3fc4aa8bb4
Arthur Pimenta <arthurstt22@gmail.com>
2022-04-25 13:59:34 -0300

    Modules organization

--- script.js
+++ script.js
@@ -1,84 +1,9 @@
-function initTabNav() {
-    const tabMenu = document.querySelectorAll('[data-tab="menu"] li');
-    const tabContent = document.querySelectorAll('[data-tab="content"] section');
-
-    if(tabContent.length && tabMenu.length) {
-        tabContent[0].classList.add('ativo');
-
-        function activeTab(index) {
-            tabContent.forEach((section) => {
-            section.classList.remove('ativo');
-            });
-
-            const direcao = tabContent[index].dataset.anime;
-            tabContent[index].classList.add('ativo', direcao);
-        }
-
-        tabMenu.forEach((itemMenu, index) => {
-            itemMenu.addEventListener('click', () => {
-            activeTab(index);
-            });
-        });
-    }
-}
-initTabNav();
-
-function initAccordion() {
-    const accordionList = document.querySelectorAll('[data-anime="accordion"] dt');
-    accordionList[0].classList.add('ativo');
-    accordionList[0].nextElementSibling.classList.add('ativo');
-
-    function activeAccordion() {
-        this.classList.toggle('ativo');
-        this.nextElementSibling.classList.toggle('ativo') ;
-    }
-
-    accordionList.forEach((item) => {
-        item.addEventListener('click', activeAccordion);
-    });
-}
-initAccordion();
-
-function initscrollSuave() {
-    const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
-
-    function scrollToSection(event) {
-        event.preventDefault();
-        const href = event.currentTarget.getAttribute('href');
-        const section = document.querySelector(href);
-        const topo = section.offsetTop;
-
-        window.scrollTo({
-            top: topo,
-            behavior: 'smooth',
-        });
-    }
-
-    linksInternos.forEach((link) => {
-        link.addEventListener('click', scrollToSection);
-    });
-}
+import initscrollSuave from './js/modules/scroll-suave.js';
+import initScroll from './js/modules/scroll.js';
+import initAccordion from './js/modules/accordion.js';
+import initTabNav from './js/modules/tab.js';
 
 initscrollSuave();
-
-function initScroll() {
-    const sections = document.querySelectorAll('[data-anime="scroll"]');
-    if(sections.length) {
-        const windowMetade = window.innerHeight * 0.6;
-
-        function animaScroll() {
-            sections.forEach((section) => {
-                const sectionTop = section.getBoundingClientRect().top;
-                const isSectionVisible = (sectionTop - windowMetade) < 0;
-                if(isSectionVisible) {
-                    section.classList.add('ativo');
-                }
-            })
-        }
-
-        animaScroll();
-
-        window.addEventListener('scroll', animaScroll);
-    }
-}
 initScroll();
+initAccordion();
+initTabNav();
